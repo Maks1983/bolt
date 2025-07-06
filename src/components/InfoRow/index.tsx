89433da8f170fbbec4c48355bb458aa61c9e39ab
Maks1983 <Maks1983@hotmail.com>
2025-07-06 11:13:14 +0200

    Updated index.tsx

--- src/components/InfoRow/index.tsx
+++ src/components/InfoRow/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import CompactStatsRow from './CompactStatsRow';
-import QuickActionsBar from './QuickActionsBar';
+import AlarmCard from './AlarmCard';
+import SecurityCard from './SecurityCard';
+import EnergyCard from './EnergyCard';
+import IndoorCard from './IndoorCard';
+import ActivityCard from './ActivityCard';
+import ActionsCard from './ActionsCard';
+import NVRCard from './NVRCard';
 
 const InfoRow: React.FC = () => {
   return (
     <div className="px-6 py-4">
-      <div className="max-w-7xl mx-auto space-y-4">
-        {/* Compact Stats Row */}
-        <CompactStatsRow />
-        
-        {/* Quick Actions Bar */}
-        <QuickActionsBar />
+      <div className="flex items-center justify-center">
+        <div className="flex space-x-4 overflow-x-auto scrollbar-hide">
+          <AlarmCard />
+          <SecurityCard />
+          <EnergyCard />
+          <IndoorCard />
+          <ActivityCard />
+          <ActionsCard />
+          <NVRCard />
+        </div>
       </div>
     </div>
   );
