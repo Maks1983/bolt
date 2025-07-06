06cb1e37c89aebd9df0b6ff806e7e9af90092d45
Maks1983 <Maks1983@hotmail.com>
2025-07-06 12:54:42 +0200

    Fix InfoRow component export

--- src/components/InfoRow.tsx
+++ src/components/InfoRow.tsx
@@ -1,9 +1,24 @@
-<>
-  <NVRCard onClick={() => console.log('NVR clicked')} />
-  <ActionsCard />
-  <ActivityCard />
-  <IndoorAverageCard />
-  <EnergyCard />
-  <SecurityCard />
-  <AlarmCard />
-</>
+import React from 'react';
+import { NVRCard } from './InfoCards/NVRCard';
+import { ActionsCard } from './InfoCards/ActionsCard';
+import { ActivityCard } from './InfoCards/ActivityCard';
+import { IndoorAverageCard } from './InfoCards/IndoorAverageCard';
+import { EnergyCard } from './InfoCards/EnergyCard';
+import { SecurityCard } from './InfoCards/SecurityCard';
+import { AlarmCard } from './InfoCards/AlarmCard';
+
+const InfoRow: React.FC = () => {
+  return (
+    <>
+      <NVRCard onClick={() => console.log('NVR clicked')} />
+      <ActionsCard />
+      <ActivityCard />
+      <IndoorAverageCard />
+      <EnergyCard />
+      <SecurityCard />
+      <AlarmCard />
+    </>
+  );
+};
+
+export default InfoRow;
