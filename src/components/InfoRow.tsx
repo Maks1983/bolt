81c996bc5273ec849d0e2bb7d0b9af38c6e990d6
Maks1983 <Maks1983@hotmail.com>
2025-07-06 12:56:51 +0200

    Fix import/export mismatches in InfoRow

--- src/components/InfoRow.tsx
+++ src/components/InfoRow.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NVRCard } from './InfoCards/NVRCard';
+import NVRCard from './InfoCards/NVRCard';
 import ActionsCard from './InfoCards/ActionsCard';
 import ActivityCard from './InfoCards/ActivityCard';
-import { IndoorAverageCard } from './InfoCards/IndoorAverageCard';
+import IndoorAverageCard from './InfoCards/IndoorAverageCard';
 import EnergyCard from './InfoCards/EnergyCard';
-import { SecurityCard } from './InfoCards/SecurityCard';
+import SecurityCard from './InfoCards/SecurityCard';
 import AlarmCard from './InfoCards/AlarmCard';
 
 const InfoRow: React.FC = () => {
