f1395ed8a8e5ed6eeeaa094050cb6c2b76428e58
Maks1983 <Maks1983@hotmail.com>
2025-07-06 12:53:52 +0200

    Extract existing InfoRow cards into separate files

--- src/components/InfoRow.tsx
+++ src/components/InfoRow.tsx
@@ -1,43 +1,7 @@
-import React from 'react';
-import AlarmCard from './InfoCards/AlarmCard';
-import SecurityCard from './InfoCards/SecurityCard';
-import EnergyCard from './InfoCards/EnergyCard';
-import IndoorAverageCard from './InfoCards/IndoorAverageCard';
-import ActivityCard from './InfoCards/ActivityCard';
-import ActionsCard from './InfoCards/ActionsCard';
-import NVRCard from './InfoCards/NVRCard';
-
-interface Camera {
-  id: number;
-  name: string;
-  location: string;
-  recording: boolean;
-  nightVision: boolean;
-  temperature: number;
-  humidity: number;
-  backgroundImage: string;
-}
-
-interface InfoRowProps {
-  cameras: Camera[];
-}
-
-const InfoRow: React.FC<InfoRowProps> = ({ cameras }) => {
-  return (
-    <div className="px-6 py-4">
-      <div className="overflow-x-auto scrollbar-hide">
-        <div className="flex space-x-4 min-w-max">
-          <AlarmCard />
-          <SecurityCard />
-          <EnergyCard />
-          <IndoorAverageCard />
-          <ActivityCard />
-          <ActionsCard />
           <NVRCard onClick={() => console.log('NVR clicked')} />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default InfoRow;
+          <ActionsCard />
+          <ActivityCard />
+          <IndoorAverageCard />
+          <EnergyCard />
+          <SecurityCard />
+          <AlarmCard />
