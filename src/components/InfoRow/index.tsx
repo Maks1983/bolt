6bc63ee6b5215eff4098dfb832ab698dfce56c90
Maks1983 <Maks1983@hotmail.com>
2025-07-06 11:07:14 +0200

    Redesign InfoRow to be more compact and space-efficient

--- src/components/InfoRow/index.tsx
+++ src/components/InfoRow/index.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import SystemOverviewCard from './SystemOverviewCard';
-import QuickStatsGrid from './QuickStatsGrid';
-import AlertsAndNotifications from './AlertsAndNotifications';
-import QuickActionsPanel from './QuickActionsPanel';
+import CompactStatsRow from './CompactStatsRow';
+import QuickActionsBar from './QuickActionsBar';
 
 const InfoRow: React.FC = () => {
   return (
-    <div className="px-6 py-6 bg-gradient-to-r from-slate-50 via-blue-50/30 to-indigo-50/50">
-      {/* Main Dashboard Overview */}
-      <div className="max-w-7xl mx-auto space-y-6">
+    <div className="px-6 py-4">
+      <div className="max-w-7xl mx-auto space-y-4">
+        {/* Compact Stats Row */}
+        <CompactStatsRow />
         
-        {/* Top Row - System Overview */}
-        <SystemOverviewCard />
-        
-        {/* Middle Row - Quick Stats Grid */}
-        <QuickStatsGrid />
-        
-        {/* Bottom Row - Alerts and Quick Actions */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="lg:col-span-2">
-            <AlertsAndNotifications />
-          </div>
-          <div>
-            <QuickActionsPanel />
-          </div>
-        </div>
+        {/* Quick Actions Bar */}
+        <QuickActionsBar />
       </div>
     </div>
   );
